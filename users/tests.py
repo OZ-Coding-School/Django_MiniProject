8b6b75280407c18f4dd1107d6e352fbdd2fd060e
meoyoug <eodyd426@gmail.com>
2024-08-22 14:45:31 +0900

    커스텀 유저 모델 선언 및 테스트 코드 수정

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class UserModelTestCase(TestCase):
