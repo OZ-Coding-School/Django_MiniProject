ba257ede6f1b10c5a9a55820d43a30d802e3dc56
meoyoug <eodyd426@gmail.com>
2024-08-22 10:00:14 +0900

    Feat: User, Transaction, Account 모델 테스트 코드 추가

--- transactions/tests.py
+++ transactions/tests.py
@@ -1,3 +1,81 @@
+from datetime import datetime
+
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+
+
+class TransactionTestCase(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            email="test@example.com",
+            password="testpassword1234",
+            nickname="testuser",
+            name="홍길동",
+            phone="010-1111-2222",
+        )
+        self.account = Account.objects.create(
+            user_id=self.user.id,
+            account_num='3333-54-1231231',
+            bank_code='090',
+            balance=1000000,
+            type='입출금통장'
+        )
+        self.transaction_data = {
+            'account_id': self.account.id,
+            'trans_amount': 18000,
+            'after_balance': 982000,
+            'print_content': '자동이체',
+            'trans_type': '출금',
+            'trans_method': '유튜브 정기결제',
+            'trans_date': datetime.now(),
+            'trans_time': datetime.now().time()
+        }
+
+    def test_transaction_create(self):
+        transaction = Transaction.objects.create(**self.transaction_data)
+
+        self.assertEqual(transaction.account_id, self.transaction_data['account_id'])
+        self.assertEqual(transaction.trans_amount, self.transaction_data['trans_amount'])
+        self.assertEqual(transaction.after_balance, self.transaction_data['after_balance'])
+        self.assertEqual(transaction.print_content, self.transaction_data['print_content'])
+        self.assertEqual(transaction.trans_type, self.transaction_data['trans_type'])
+        self.assertEqual(transaction.trans_method, self.transaction_data['trans_method'])
+        self.assertEqual(transaction.trans_date, self.transaction_data['trans_date'])
+        self.assertEqual(transaction.trans_time, self.transaction_data['trans_time'])
+
+    def test_transaction_update(self):
+        transaction = Transaction.objects.create(**self.transaction_data)
+        transaction.trans_amount = 20000
+
+        transaction.save()
+
+        transaction.refresh_from_db()
+        self.assertEqual(transaction.trans_amount, 20000)
+        self.assertEqual(transaction.after_balance, 980000)
+
+    def test_transaction_delete(self):
+        transaction = Transaction.objects.create(**self.transaction_data)
+
+        transaction.delete()
+
+        self.assertFalse(Transaction.objects.filter(id=transaction.id).exists())
+
+    def test_transaction_delete_when_account_deleted(self):
+        account = Account.objects.get(id=self.account.id)
+        Transaction.objects.create(**self.transaction_data)
+
+        account.delete()
+
+        self.assertFalse(Account.objects.filter(id=self.account.id).exists())
+        self.assertFalse(Transaction.objects.filter(account_id=self.account.id).exists())
+
+    def test_transaction_delete_when_user_deleted(self):
+        user = get_user_model().objects.get(id=self.user.id)
+        Transaction.objects.create(**self.transaction_data)
+
+        user.delete()
+
+        self.assertFalse(get_user_model().objects.filter(id=self.user.id).exists())
+        self.assertFalse(Account.objects.filter(user_id=self.user.id).exists())
+        self.assertFalse(Transaction.objects.filter(account__user_id=self.user.id).exists())
--- users/tests.py
+++ users/tests.py
@@ -4,18 +4,47 @@ from django.contrib.auth.models import User
 
 class UserModelTestCase(TestCase):
 	def setUp(self):
-		# Given : 아래와 같은 유저 데이터를 입력 받았을 때
 		self.user_data = {
 			'email': 'test@example.com',
+			'password': 'testpassword1234',
 			'nickname': 'testuser',
-			'password': 'testpassword1234'
+			'name': '홍길동',
+			'phone': '010-1111-2222'
 		}
 
 	def test_create_user(self):
-		# When : 입력 받은 데이터를 바탕으로 유저 모델을 생성하면
 		user = User.objects.create_user(**self.user_data)
 
-		# Then : 생성된 유저 모델의 이메일, 닉네임, 비밀번호가 입력받은 데이터와 일치함을 검증
 		self.assertEqual(user.email, self.user_data['email'])
 		self.assertEqual(user.nickname, self.user_data['nickname'])
 		self.assertTrue(user.check_password(self.user_data['password']))
+		self.assertFalse(user.is_staff)
+		self.assertFalse(user.is_superuser)
+		self.assertFalse(user.is_active)
+
+	def test_create_superuser(self):
+		superuser = User.objects.create_superuser(**self.user_data)
+
+		self.assertEqual(superuser.email, self.user_data['email'])
+		self.assertEqual(superuser.nickname, self.user_data['nickname'])
+		self.assertTrue(superuser.check_password(self.user_data['password']))
+		self.assertTrue(superuser.is_staff)
+		self.assertTrue(superuser.is_superuser)
+		self.assertTrue(superuser.is_active)
+
+	def test_update_user(self):
+		user = User.objects.create_user(**self.user_data)
+
+		user.nickname = 'updateduser'
+		user.save()
+
+		user.refresh_from_db()
+		self.assertEqual(user.nickname, 'updateduser')
+
+	def test_delete_user(self):
+		user = User.objects.create_user(**self.user_data)
+		self.assertEqual(User.objects.count(), 1)
+
+		user.delete()
+
+		self.assertEqual(User.objects.count(), 0)
