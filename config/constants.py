34cd1376ac93b93a28c78fc06cacfd08ee024134
meoyoug <eodyd426@gmail.com>
2024-08-22 17:27:57 +0900

    거래 종류, 타입 상수 지정

--- config/constants.py
+++ config/constants.py
@@ -103,3 +103,18 @@ ACCOUNT_TYPE = [
     ("IRP", "퇴직연금"),
     ("STOCK", "주식"),
 ]
+
+# 거래 타입
+TRANSACTION_TYPE = [
+    ("DEPOSIT", "입금"),
+    ("WITHDRAW", "출금"),
+]
+
+# 거래 종류
+TRANSACTION_METHOD = [
+    ("ATM", "ATM 거래"),
+    ("TRANSFER", "계좌이체"),
+    ("AUTOMATIC_TRANSFER", "자동이체"),
+    ("CARD", "카드결제"),
+    ("INTEREST", "이자"),
+]
