e8fea30c06e7c3915df647e697d025b4c04e60c8
meoyoug <eodyd426@gmail.com>
2024-08-26 10:48:54 +0900

    Feat: JWT Login, Logout TestCode Edit
    - JWT Login Success Case
    - JWT Login Failure Case
    - JWT Logut Success Case

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
 User = get_user_model()
 
@@ -50,3 +51,74 @@ class UserModelTestCase(TestCase):
         user.delete()
 
         self.assertEqual(User.objects.count(), 0)
+
+
+class UserViewTestCase(APITestCase):
+    def setUp(self):
+        self.user_data = {
+            "email": "test@example.com",
+            "password": "testpassword1234",
+            "nickname": "testuser",
+            "name": "홍길동",
+            "phone": "010-1111-2222",
+        }
+        self.user = User.objects.create_user(**self.user_data)
+
+    def test_user_jwt_login(self):
+        response = self.client.post(
+            "users/login/",
+            {
+                "email": self.user_data["email"],
+                "password": self.user_data["password"]
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("access", response.cookies)
+        self.assertIn("refresh", response.cookies)
+        self.assertEqual(response.data['detail'], "Login successful")
+
+    def test_jwt_login_failure(self):
+        # 잘못된 자격 증명으로 로그인 시도
+        response = self.client.post(
+            "users/login/",
+            {
+                "email": self.user_data["email"],
+                'password': 'wrongpassword'
+            }
+        )
+
+        # 응답 상태 코드 확인
+        self.assertEqual(response.status_code, 401)
+
+        # 쿠키가 설정되지 않았는지 확인
+        self.assertNotIn('access', response.cookies)
+        self.assertNotIn('refresh', response.cookies)
+
+        # 응답 데이터 확인
+        self.assertEqual(response.data['detail'], "Invalid credentials")
+
+    def test_jwt_logout(self):
+        response = self.client.post(
+            "users/login/",
+            {
+                "email": self.user_data["email"],
+                "password": self.user_data["password"]
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("access", response.cookies)
+        self.assertIn("refresh", response.cookies)
+
+        response = self.client.post("users/logout/")
+
+        # 상태코드 확인
+        self.assertEqual(response.status_code, 200)
+
+        # 쿠키가 삭제되었는지 확인
+        self.assertNotIn("access", response.cookies)
+        self.assertNotIn("refresh", response.cookies)
+
+        # 응답 메시지 확인
+        self.assertEqual(response.data['detail'], "Logout successful")
