867d95b9dbb7b804b2e6dd877d8be175b8330c3c
meoyoug <eodyd426@gmail.com>
2024-09-03 11:42:37 +0900

    Feat : Account CRUD RestAPI 구현 및 Serializer 구현
    - views.py
      - AccountListCreateView(ListCreateAPIView) : 사용자가 보유한 계좌 등록 및 리스트 조회
      - AccountDetailView(RetrieveUpdateDestroyAPIView) : 사용자가 등록한 계좌의 상세 조회 및 수정, 삭제 기능
    
    - serializers.py
      - AccountSerializer(serializers.ModelSerializer) : 계좌 리스트 조회 및 등록시 사용할 시리얼라이저
        - to_representation(self, instance) : Choice Field 를 사용자 친화적인 문자열로 반환하도록 설정
      - AccountDetailSerializer(AccountSerializer) : 계좌 상세 조회 및 업데이트시 사용되는 시리얼라이저

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from accounts.models import Account
+from accounts.serializers import AccountSerializer, AccountDetailSerializer
 
-# Create your views here.
+
+class AccountListCreateView(ListCreateAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class AccountDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountDetailSerializer
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
