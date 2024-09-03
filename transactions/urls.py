4ffc24faf38f7f7ad683f08d25eecac2e7ac85b0
meoyoug <eodyd426@gmail.com>
2024-09-03 11:32:25 +0900

    Feat: Transaction CRUD View와 URL 매핑

---
+++ transactions/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from transactions import views as trans_views
+
+urlpatterns = [
+    path("", trans_views.TransactionListCreateView.as_view(), name="transaction-list"),
+    path("<int:pk>/", trans_views.TransactionDetailView.as_view(), name="transaction-detail"),
+]
